588cac636445883f04be5a27969d666fdd522770
Min ho Kim <minho42@gmail.com>
2022-04-21 19:05:36 +1000

    sticky navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
   const router = useRouter();
 
   return (
-    <nav className="flex w-full items-center justify-center sm:justify-around bg-white  border-b border-gray-300 h-12 mb-3 px-2 sm:px-6">
+    <nav className="sticky top-0 z-50 flex w-full items-center justify-center sm:justify-around bg-white  border-b border-gray-300 h-12 mb-3 px-2 sm:px-6">
       <div className="text-2xl">
         <Link href="/">Beach ⛱</Link>
       </div>
--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -1,13 +1,29 @@
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, XIcon } from "@heroicons/react/outline";
 
 export function Search({ setSearchText }) {
+  const searchInput = document.querySelector("input");
+
   const handleChange = (e) => {
-    setSearchText(e.target.value);
+    if (e.target.value.trim().length === 0) return;
+
+    setSearchText(e.target.value.trim());
+  };
+
+  const handleDelete = (e) => {
+    searchInput.focus();
+    searchInput.value = "";
+    setSearchText("");
   };
 
   return (
     <div className="flex w-full max-w-sm relative px-2">
       <SearchIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400" />
+
+      {searchInput && searchInput.value.length > 0 && (
+        <button onClick={handleDelete}>
+          <XIcon className="absolute right-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400 hover:text-gray-500" />
+        </button>
+      )}
       <input
         onChange={handleChange}
         type="text"
