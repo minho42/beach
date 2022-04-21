29941a47aa336ee929e75e7d195a6200a8b537c1
Min ho Kim <minho42@gmail.com>
2022-04-21 23:20:39 +1000

    style

--- frontend/components/BeachItem.js
+++ frontend/components/BeachItem.js
@@ -1,11 +1,9 @@
-import { ExternalLinkIcon } from "@heroicons/react/outline";
-
 export function BeachItem({ data: { name, stars, reviews, ranking, imageSrc }, index }) {
   const url = `https://www.google.com.au/search?q=${name.replace(" ", "+")}`;
   const imageUrl = `${url}&tbm=isch`;
   return (
-    <section className="flex justify-center w-full">
-      <div className="flex flex-col w-full items-center bg-white rounded-xl px-6 pt-4 pb-6 space-x-3 space-y-2 border border-transparent shadow hover:shadow-lg ">
+    <section className="flex justify-center w-full group">
+      <div className="flex flex-col w-full items-center bg-white rounded-xl px-6 pt-4 pb-6 space-x-3 space-y-2 border border-gray-300 hover:shadow-lg ">
         <div className="relative rounded-xl overflow-hidden">
           <a
             href={imageUrl}
@@ -17,26 +15,25 @@ export function BeachItem({ data: { name, stars, reviews, ranking, imageSrc }, i
           >
             <img
               src={imageSrc}
-              className="w-72 h-auto object-contain transform transition duration-1000 hover:scale-125  "
+              className="w-72 h-auto object-contain transform transition duration-1000 ease-in-out group-hover:scale-110"
             />
           </a>
-          <div className="absolute top-1 left-1 text-6xl font-bold text-white text-shadow">{index + 1}</div>
+          <div className="absolute top-0.5 left-1 text-5xl font-bold text-white text-shadow">{index + 1}</div>
         </div>
 
-        <div className="flex flex-col space-y-2">
+        <div className="flex flex-col items-center w-full space-y-2">
           <a
             href={url}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center text-center text-3xl capitalize hover:underline"
+            className="text-center text-2xl font-semibold capitalize hover:underline"
             alt={`Google ${name}`}
             title={`Google ${name}`}
           >
             {name}
-            <ExternalLinkIcon className="w-7 h-7 text-gray-400 ml-2" />
           </a>
-          <div className="flex items-center space-x-2">
-            <div className="bg-amber-200 rounded-lg px-2">{stars}</div>
+          <div className="flex w-full items-center justify-center text-base space-x-2">
+            <div className="bg-gray-200 rounded-lg px-2">{stars}</div>
             <div>·</div>
             <div className="text-gray-600">{reviews} reviews</div>
           </div>
