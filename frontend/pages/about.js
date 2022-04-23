e69e2df3e66bab46650930b89694cb65915de625
Min ho Kim <minho42@gmail.com>
2022-04-24 02:25:50 +1000

    add scraping date, description

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -3,9 +3,9 @@ export default function About() {
     <div className="flex flex-col items-center justify-center space-y-6">
       <h1 className="text-center text-3xl">Best beaches in Sydney</h1>
 
-      <p></p>
+      <p>Beaches in Sydney ranked by Google reviews</p>
 
-      <p></p>
+      <p>Updated 21 Apr 2022</p>
     </div>
   );
 }
